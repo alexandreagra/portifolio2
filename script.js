e94b620c766cdca2d143c73114cf040db3cce68a
AlexandreDev <alexandreagradev@gmail.com>
2022-11-12 11:29:41 -0300

    navJS

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const head = document.querySelector('#header-global')
-let scrollBar = window.scrollY
+const body = document.body
+let lastScroll = 0
 
-window.addEventListener("scroll", () => {
-    if(scrollBar < window.scrollY) {
-        nav.classList.add('header-global')
-    } else {
-        nav.classList.remove('header-global')
+window.addEventListener('scroll', () =>{
+    const currentScroll = window.scrollY
+
+    if(currentScroll <= 0){
+        body.classList.remove('scroll-up')
     }
 
-    scrollBar = window.scrollY
+    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-up')
+        body.classList.add('scroll-down')
+    }
+
+    if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-down')
+        body.classList.add('scroll-up')
+    }
+
+    lastScroll = currentScroll
 })
